ac53e6ed091df0cb14b869a7a72368f79c549a8d
alanhunt1 <30370570+alanhunt1@users.noreply.github.com>
2021-01-28 10:35:28 -0500

    merging Jon's changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+/*
+  App.js is the starting point for the application.   All of the components in your app should have this file as the root.
+  This is the level that will handle the routing of requests, and also the one that will manage communication between
+  sibling components at a lower level.  It holds the basic structural components of navigation, content, and a modal dialog.
+*/
+
 import React from "react";
 import "./App.css";
 import PostForm from "./Component/PostForm.js";
@@ -11,13 +17,20 @@ import {
   BrowserRouter as Router, Route, Switch, Link
 } from 'react-router-dom';
 
+// toggleModal will both show and hide the modal dialog, depending on current state.  Note that the
+// contents of the modal dialog are set separately before calling toggle - this is just responsible
+// for showing and hiding the component
 function toggleModal(app) {
   app.setState({
     openModal: !app.state.openModal
   });
 }
 
+// the App class defines the main rendering method and state information for the app
 class App extends React.Component {
+
+  // the only state held at the app level is whether or not the modal dialog
+  // is currently displayed - it is hidden by default when the app is started.
   constructor(props) {
     super(props);
     this.state = {
@@ -26,19 +39,27 @@ class App extends React.Component {
   }
 
   render() {
-    let post = require("./post.svg");
-    let friend = require("./friends.svg");
-    let setting = require("./settings.svg");
-    let help = require("./help.svg");
+
+    // in the event we need a handle back to the parent from a child component,
+    // we can create a reference to this and pass it down.
     let mainContent = React.createRef();
 
     return (
+
+      // the app is wrapped in a router component, that will render the
+      // appropriate content based on the URL path.  Since this is a
+      // single page app, it allows some degree of direct linking via the URL
+      // rather than by parameters
       <Router>
       <div className="App">
         <header className="App-header">
 
+          // create the side navigation bar - this stays constant
+          // regardless of the routing
           <Navbar toggleModal={e => toggleModal(this, e)} />
 
+          // create the main content area.  This will change based on
+          // the routing.
           <div className="maincontent" id="mainContent">
             <Switch>
             <Route path="/posts">
@@ -61,6 +82,8 @@ class App extends React.Component {
                 <FriendList userid={sessionStorage.getItem("user")} />
               </div>
             </Route>
+            // the "default" route - in our case, it mirrors the /posts route.
+            // this is a good place to put a login check.
             <Route path="/">
               <div>
                 <p>Social Media Home</p>
@@ -71,6 +94,8 @@ class App extends React.Component {
             </Switch>
           </div>
         </header>
+
+        // the modal dialog
         <Modal show={this.state.openModal} onClose={e => toggleModal(this, e)}>
           This is a modal dialog!
         </Modal>
@@ -80,4 +105,5 @@ class App extends React.Component {
   }
 }
 
+// export the app for use in index.js
 export default App;
