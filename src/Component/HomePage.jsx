5b3df2f24c6ccce95a54ecc8526808db7e941477
Justin Variara <jrvariar@buffalo.edu>
2023-09-12 13:51:46 -0400

    Added Posts, Settings and HomePage files in Component and updated App.jsx to be functional

---
+++ src/Component/HomePage.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import LoginForm from "./LoginForm";
+import Posts from "./Posts";
+
+const HomePage = ({ isLoggedIn }) => {
+  // state variable for userToken, intiially set to an empty string
+  const [userToken, setUserToken] = useState("");
+
+  // useEffect hook, this will run everything inside the callback
+  // function once when the component loads
+  useEffect(() => {
+    setUserToken(sessionStorage.getItem("token"));
+  }, []);
+
+  // if the user is not logged in, show the login form.  Otherwise, show the post form
+  return (
+    <div>
+      {!userToken ? (
+        <LoginForm login={isLoggedIn} />
+      ) : (
+        <>
+          <Posts />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/Component/Posts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostForm from './PostForm'
+
+const Posts = () => {
+  return (
+    <div>
+       <p>CSE 370 Social Media Test Harness</p>
+       <PostForm/>
+    </div>
+  )
+}
+
+export default Posts
